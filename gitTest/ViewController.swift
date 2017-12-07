375d3b76513598dbfd57be9c9027c1c55572cf11
Vittorio Grimaldi <vitt.grim@gmail.com>
2017-12-07 17:31:14 +0100

    added comment

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // comment and commit
+        // comment and commit :D
     }
 
     override func didReceiveMemoryWarning() {
